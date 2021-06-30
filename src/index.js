842c909441944d37e053c1689200920fcd67ddcb
vishwasrajbatham <vishwasrajbatham.151@gmail.com>
2021-06-30 16:12:05 +0530

    index.css added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FrontPart from "./Components/FrontPart"
-import "index.css";
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
