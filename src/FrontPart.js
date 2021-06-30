cefba1496185d6d6ebe18ef8c43dc681b1b0d034
vishwasrajbatham <vishwasrajbatham.151@gmail.com>
2021-06-30 07:26:18 +0530

    FrontPart File Added

---
+++ src/FrontPart.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function FrontPart(){
+    return(
+        <h1>Hi ! This is frontend part. Edit this code for frontend part</h1>
+    )
+}
+
+export default FrontPart
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FrontPart from "./FrontPart"
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello world</h1>
+    <FrontPart/>
   </React.StrictMode>,
   document.getElementById('root')
 );
