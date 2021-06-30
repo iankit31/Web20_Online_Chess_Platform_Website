4c88affdab919f4e232ec88d120a7b9049d5efac
vishwasrajbatham <vishwasrajbatham.151@gmail.com>
2021-06-30 15:59:00 +0530

    Temp Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FrontPart from "./FrontPart"
+import FrontPart from "./Components/FrontPart"
+import "index.css";
 
 ReactDOM.render(
   <React.StrictMode>
