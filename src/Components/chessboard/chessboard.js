4e1fb5a39918858e08da21d5481a92c443bb48e3
Rahul <rahuljas2050@gmail.com>
2021-07-04 09:16:08 +0530

    when we drop pieces it goes to the center of the tile

--- src/Components/chessboard/chessboard.js
+++ src/Components/chessboard/chessboard.js
@@ -1,102 +1,159 @@
-import React , {useRef} from "react"
+import React, { useRef } from "react"
+import { useState } from "react"
 import "./chessboard.css"
 import Tile from "../tile/tile"
 
 
-let board = [];
+
 
 let horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
 let verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
-let pieces = [];
-let activePiece: HTMLElement | null = null;
+const initialBoard = [];
 
-for(let row=0; row<=7; row++) {    
-    for(let col=0; col<=7; col++) {
-        board.push(<span>
-            {/* {horizontalAxis[row]} {verticalAxis[7-col]}  */}
-            <Tile row={row} col={col} id={horizontalAxis[row] + verticalAxis[7-row]} /></span>)
-    }
-}
+initialBoard.push({ image:"images/br.png", x:0, y:0 });
+initialBoard.push({ image:"images/br.png", x:0, y:7 });
+initialBoard.push({ image:"images/bn.png", x:0, y:1 });
+initialBoard.push({ image:"images/bn.png", x:0, y:6 });
+initialBoard.push({ image:"images/bb.png", x:0, y:2 });
+initialBoard.push({ image:"images/bb.png", x:0, y:5 });
+initialBoard.push({ image:"images/bq.png", x:0, y:3 });
+initialBoard.push({ image:"images/bk.png", x:0, y:4 });
+
+initialBoard.push({ image:"images/wr.png", x:7, y:0 });
+initialBoard.push({ image:"images/wr.png", x:7, y:7 });
+initialBoard.push({ image:"images/wn.png", x:7, y:1 });
+initialBoard.push({ image:"images/wn.png", x:7, y:6 });
+initialBoard.push({ image:"images/wb.png", x:7, y:2 });
+initialBoard.push({ image:"images/wb.png", x:7, y:5 });
+initialBoard.push({ image:"images/wq.png", x:7, y:3 });
+initialBoard.push({ image:"images/wk.png", x:7, y:4 });
+
+for (let i = 0; i < 8; i++) { initialBoard.push({ image:"images/bp.png", x:1, y:i }) };
+for (let i = 0; i < 8; i++) { initialBoard.push({ image:"images/wp.png", x:6, y:i }) };
+
 
 function Chessboard() {
-const chessBoardRef=useRef(null);
+    const chessBoardRef = useRef(null);
+    const [pieces, setPieces] = useState(initialBoard);
+    const [initialX, setInitialX] = useState(null);
+    const [initialY, setInitialY] = useState(null);
+    const [activePiece, setActivePiece] = useState(null);
+    // console.log(pieces);
 
-
-
-
-
-function grabPiece(e:React.MouseEvent){
-    const element = e.target 
-    if(element.classList.contains("chess-piece")){
-        console.log(e);
-
-        const x = e.clientX-35;
-        const y = e.clientY-35;
-        element.style.position = "absolute";
-        element.style.left = `${x}px`
-        element.style.top = `${y}px`
-
-        activePiece = element;
-    }
-    //console.log(e.target)
-}
-
-function movePiece(e:React.MouseEvent){
-
-    const chessboard=chessBoardRef.current;
-    
-    if(activePiece && activePiece.classList.contains("chess-piece")&&chessboard){
-
-        //console.log(e);
-        const minX=chessboard.offsetLeft-15;
-        const minY=chessboard.offsetTop-15;
-        const maxX=chessboard.offsetLeft+chessboard.clientWidth-60;
-        const maxY=chessboard.offsetTop+chessboard.clientHeight-60;
-        const x = e.clientX-35;
-        const y = e.clientY-35;
-        activePiece.style.position = "absolute";
-
-        if(x<minX){
-            activePiece.style.left = `${minX}px`
+    let board = [];
+    for (let row = 0; row <= 7; row++) {
+        for (let col = 0; col <= 7; col++) {
+            board.push(<span>
+                {/* {horizontalAxis[row]} {verticalAxis[7-col]}  */}
+                <Tile 
+                    pieces={pieces} 
+                    row={row} 
+                    col={col} 
+                    key={`horizontalAxis[row],verticalAxis[7 - row]`} /></span>)
         }
-        else if(x>maxX){
-            activePiece.style.left = `${maxX}px`
-        }
-        else{
-            activePiece.style.left = `${x}px`
-        }
-
-        if(y<minY){
-            activePiece.style.top = `${minY}px`
-        }
-        else if(y>maxY){
-            activePiece.style.top = `${maxY}px`
-        }
-        else{
-            activePiece.style.top = `${y}px`
-        }
-        
-    }
-    //console.log(e.target)
-}
-
-function dropPiece(e:React.MouseEvent){
-    if(activePiece){
-        activePiece = null;
     }
 
-}
+    function grabPiece(e) {
+        const chessboard = chessBoardRef.current;
+        const element = e.target
+        if (element.classList.contains("chess-piece")) {
+            setInitialY(Math.floor((e.clientX - chessboard.offsetLeft)/70));
+            setInitialX(Math.floor((e.clientY - chessboard.offsetTop)/70));
+            // console.log(e);
+
+            const x = e.clientX - 35;
+            const y = e.clientY - 35;
+            element.style.position = "absolute";
+            element.style.left = `${x}px`
+            element.style.top = `${y}px`
+
+            setActivePiece(element);
+        }
+        //console.log(e.target)
+    }
+
+    function movePiece(e) {
+
+        const chessboard = chessBoardRef.current;
+
+        if (activePiece && chessboard) {
+
+            //console.log(e);
+            const minX = chessboard.offsetLeft - 15;
+            const minY = chessboard.offsetTop - 15;
+            const maxX = chessboard.offsetLeft + chessboard.clientWidth - 60;
+            const maxY = chessboard.offsetTop + chessboard.clientHeight - 60;
+            const x = e.clientX - 35;
+            const y = e.clientY - 35;
+            activePiece.style.position = "absolute";
+
+            if (x < minX) {
+                activePiece.style.left = `${minX}px`
+            }
+            else if (x > maxX) {
+                activePiece.style.left = `${maxX}px`
+            }
+            else {
+                activePiece.style.left = `${x}px`
+            }
+
+            if (y < minY) {
+                activePiece.style.top = `${minY}px`
+            }
+            else if (y > maxY) {
+                activePiece.style.top = `${maxY}px`
+            }
+            else {
+                activePiece.style.top = `${y}px`
+            }
+
+        }
+        //console.log(e.target)
+    }
+
+    function dropPiece(e) {
+        const chessboard = chessBoardRef.current;
+        const col_num = Math.floor((e.clientX - chessboard.offsetLeft)/70);
+        const row_num = Math.floor((e.clientY - chessboard.offsetTop)/70);
+        const minX = chessboard.offsetLeft - 15;
+            const minY = chessboard.offsetTop - 15;
+            const maxX = chessboard.offsetLeft + chessboard.clientWidth - 60;
+            const maxY = chessboard.offsetTop + chessboard.clientHeight - 60;
+        // console.log(initialX, initialY);
+        // console.log(row_num, col_num);
+        if (activePiece) {
+            if(e.clientX > maxX || e.clientX < minX || e.clientY > maxY || e.clientY < minY) {
+                setPieces(pieces);
+            } else{
+                setPieces( prevPieces =>{
+                    const newPieces = prevPieces.map( piece=>{
+                        if(piece.x === initialX && piece.y === initialY) {
+                            piece.x = row_num;
+                            piece.y = col_num;
+                        }
+                        return piece;
+                    } );
+                    return newPieces;
+                })
+            }
+            setActivePiece(null);
+            setInitialX(null);
+            setInitialY(null);
+        }
+
+    }
 
     return (
         <div
-          className="chessboard"
-          ref={chessBoardRef}
-          onMouseDown={ e =>grabPiece(e)}
-          onMouseMove={ e =>movePiece(e)}
-          onMouseUp={ e => dropPiece(e)}
-          >
+            className="chessboard"
+            ref={chessBoardRef}
+            onMouseDown={e => grabPiece(e)}
+            onMouseMove={e => movePiece(e)}
+            onMouseUp={e => dropPiece(e)}
+        >
             {board}
+            {/* {console.log(pieces)} */}
         </div>
     )
 }
--- src/Components/tile/tile.js
+++ src/Components/tile/tile.js
@@ -1,66 +1,28 @@
 import React from "react"
 import "./tile.css"
 
-let piece = null;
-
 function Tile(props) {
-    if(props.row === 6) {
-        piece = "images/wp.png"
-    } 
-    else if(props.row === 1) {
-        piece = "images/bp.png"
-    }
-    else if(props.row === 0) {
-        if(props.col===0 || props.col===7) {
-            piece = "images/br.png"
+    let piece = null;
+    props.pieces.forEach( p =>{
+        if(props.row === p.x && props.col === p.y) {
+            piece = p;
         }
-        if(props.col===1 || props.col===6) {
-            piece = "images/bn.png"
-        }
-        if(props.col===2 || props.col===5) {
-            piece = "images/bb.png"
-        }
-        if(props.col===3) {
-            piece = "images/bk.png"
-        }
-        if(props.col===4) {
-            piece = "images/bq.png"
-        }
-    }
-    else if(props.row === 7) {
-        if(props.col===0 || props.col===7) {
-            piece = "images/wr.png"
-        }
-        if(props.col===1 || props.col===6) {
-            piece = "images/wn.png"
-        }
-        if(props.col===2 || props.col===5) {
-            piece = "images/wb.png"
-        }
-        if(props.col===3) {
-            piece = "images/wk.png"
-        }
-        if(props.col===4) {
-            piece = "images/wq.png"
-        }
+    })
+
+    if ((props.row + props.col) % 2 === 0) {
+        return (
+            <div className="tile black-tile">
+                {piece && <div style={{ backgroundImage: `url(${piece.image})` }} className="chess-piece" ></div>}
+            </div>
+        )
     }
     else {
-        piece = null;
+        return (
+            <div className="tile white-tile">
+                {piece && <div style={{ backgroundImage: `url(${piece.image})` }} className="chess-piece" ></div>}
+            </div>
+        )
     }
-        if((props.row + props.col) %2 === 0) {
-            return (   
-                <div className="tile black-tile">
-                    {piece && <div style = {{backgroundImage: `url(${piece})`}} className="chess-piece" ></div>}
-                </div>
-            )
-        }
-        else {
-            return(
-                <div className="tile white-tile">
-                    {piece && <div style = {{backgroundImage: `url(${piece})`}} className="chess-piece" ></div>}
-                </div>
-            ) 
-        }
 }
 
 export default Tile
