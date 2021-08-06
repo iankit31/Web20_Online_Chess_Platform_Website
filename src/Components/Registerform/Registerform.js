0b5affe459ff910b3f2dbc23f199d29b4a33e79d
vishwasrajbatham <vishwasrajbatham.151@gmail.com>
2021-08-06 18:11:35 +0530

    Registration UI modified

--- src/Components/Registerform/Registerform.js
+++ src/Components/Registerform/Registerform.js
@@ -18,34 +18,31 @@ export default function Registerform() {
 
         <div className="registrationForm">
 
-            <h1 id="welcome-text">New User Register Here</h1>
-
+            <h1 className="welcome-text">Welcome to Chess ∞</h1>
+            <h1 className="welcome-text" style={{ fontSize:25, fontWeight:500}}>Register Here</h1>
+            
             <div className="form">
                 <form method="POST" action="https://ocwa.herokuapp.com/users/register">
                     <div className="form-label">
                         <label>
-                            Player Name: 	&nbsp; &nbsp;
                             <input name="name" className="form-input" type="text" value={playerName} placeholder="Enter Your Name" onChange={(e) => { setPlayerName(e.target.value) }} />
                         </label>
                     </div>
                     <br />
                     <div className="form-label">
                         <label>
-                            User Id: 	&nbsp; &nbsp;
                             <input name="id" className="form-input" type="text" value={playerId} placeholder="Enter Your unique userId" onChange={(e) => { setPlayerId(e.target.value) }} />
                         </label>
                     </div>
                     <br />
                     <div className="form-label">
                         <label>
-                            Email Id: 	&nbsp; &nbsp;
                             <input name="email" className="form-input" type="email" value={playerEmailId} placeholder="Enter Your Email Id" onChange={(e) => { setPlayerEmailId(e.target.value) }} />
                         </label>
                     </div>
                     <br />
                     <div className="form-label">
                         <label>
-                            Password: 	&nbsp; &nbsp;
                             <input name="password" className="form-input" type="password" autoComplete="on" value={playerPassword} placeholder="Enter password" onChange={(e) => { setPlayerPassword(e.target.value) }} />
                         </label>
                     </div>
@@ -54,12 +51,12 @@ export default function Registerform() {
                 </form>
             </div>
 
-            <div style={{ fontSize: 23 }}>
-                already registered, &nbsp;
+            <div className="go-back-btn">
                 <button className="submit-btn" onClick={(e) => {
                     e.preventDefault(); historyRouter.push(`/login`);
-                }}>Login Here</button>
+                }}>Go Back to Login Page</button>
             </div>
+            
         </div>
     )
 }
