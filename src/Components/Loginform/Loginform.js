116042cc51462baace10bbb94dd47ac0f847d19a
vishwasrajbatham <vishwasrajbatham.151@gmail.com>
2021-08-06 17:01:35 +0530

    Login page ui modified

--- src/Components/Loginform/Loginform.js
+++ src/Components/Loginform/Loginform.js
@@ -11,35 +11,39 @@ export default function Loginform() {
     return (
 
         <div className="loginForm">
-            <h1 id="welcome-text">Welcome to online chessgame</h1>
-            <br />
-            <h1 id="welcome-text">Login Here</h1>
-
-            <div className="form">
-                <form method="POST" action="https://ocwa.herokuapp.com/users/login">
-                    <div className="form-label">
-                        <label>
-                            User Id: 	&nbsp; &nbsp;
-                            <input name="id" className="form-input" type="text" value={playerId} placeholder="Enter Your unique userId" onChange={(e) => { setPlayerId(e.target.value) }} />
-                        </label>
+            <div className="main-div">
+                <div className="shadow-inner">
+                    <h1 id="welcome-text">Welcome to Chess Infinity</h1>
+                    <div className="form">
+                        <form method="POST" action="https://ocwa.herokuapp.com/users/login">
+                            <div className="form-label">
+                                <label>
+                                    <input name="id" className="form-input" type="text" value={playerId} placeholder="User Id" onChange={(e) => { setPlayerId(e.target.value) }} />
+                                </label>
+                            </div>
+                            <br />
+                            <div className="form-label">
+                                <label>
+                                    <input name="password" className="form-input" type="password" value={playerPassword} autoComplete="on" placeholder="Password" onChange={(e) => { setPlayerPassword(e.target.value) }} />
+                                </label>
+                            </div>
+                            <br />
+                            <input className="submit-btn" type="submit" value="Login" />
+                        </form>
                     </div>
-                    <br />
-                    <div className="form-label">
-                        <label>
-                            Password: 	&nbsp; &nbsp;
-                            <input name="password" className="form-input" type="password" value={playerPassword} autoComplete="on" placeholder="Enter password" onChange={(e) => { setPlayerPassword(e.target.value) }} />
-                        </label>
-                    </div>
-                    <br />
-                    <input className="submit-btn" type="submit" value="Login" />
-                </form>
-            </div>
 
-            <div id="regbtn">
-                If you are a new user, &nbsp;
-                <button className="submit-btn" onClick={(e) => {
-                    e.preventDefault(); historyRouter.push(`/register`);
-                }}>Register Here</button>
+                    <div id="regbtn">
+                        <h1 className="register-text">Don't have an account?</h1> &nbsp;
+                        <button className="submit-btn" onClick={(e) => {
+                        e.preventDefault(); historyRouter.push(`/register`);
+                        }}>Register Here</button>
+                    </div>
+                </div>
+                <div className="imgbox">
+                    <h1 className="Chessname">Chess</h1>
+                    <h1 className="infinity">∞</h1>
+                    <img src="/src/king.jpg" className="king-img"></img>
+                </div>
             </div>
         </div>
     )
