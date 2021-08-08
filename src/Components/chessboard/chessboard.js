3f210cd83fc836bd7dfa4a6512f18d825f56ffc6
Ankit <imankitpandey31@gmail.com>
2021-08-08 20:06:03 +0530

    remove window

--- src/Components/chessboard/chessboard.js
+++ src/Components/chessboard/chessboard.js
@@ -441,8 +441,8 @@ function Chessboard() {
                     //         jwtToken: Cookies.get('jwt'),
                     //         roomId: roomId,
                     //     })
-                    // history.push("/chessgame");
-                    window.location.href = "https://chessiiti.netlify.app/chessgame/";
+                    history.push("/chessgame");
+                    // window.location.href = "https://chessiiti.netlify.app/chessgame/";
                 }}>Exit</button>
                 {/* <Timer /> */}
             </div>
